c867d5ceb9f00adfead49050be2bc6b735ffb3a4
Dong Yoo Kang(Xellos00) <xellos.kang@gmail.com>
2021-04-21 12:16:05 +0900

    Multi processing within apis get_data  - aws-cloudwatch  - google-stackdriver  - azure-monitor

--- src/spaceone/monitoring/error/metric.py
+++ src/spaceone/monitoring/error/metric.py
@@ -4,6 +4,11 @@ from spaceone.core.error import *
 class ERROR_NOT_SUPPORT_RESOURCE_TYPE(ERROR_INVALID_ARGUMENT):
     _message = 'Data source not support resource_type. (supported_resource_type = {supported_resource_type})'
 
+class ERROR_NOT_SUPPORT_PROVIDER_MONITORING(ERROR_INVALID_ARGUMENT):
+    _message = 'Data source not support provider on monitoring. (provider = {provider})'
+
+class ERROR_NOT_MATCHING_RESOURCES(ERROR_INVALID_ARGUMENT):
+    _message = 'No Match found with given data. (monitoring = {monitoring})'
 
 class ERROR_NOT_FOUND_REFERENCE_KEY(ERROR_INVALID_ARGUMENT):
     _message = 'Reference key not found in resource. (reference_keys = {reference_keys})'
