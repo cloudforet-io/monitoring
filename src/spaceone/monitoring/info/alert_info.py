b19a0902f7129d7551ec9ea61178e12456b79b28
Jongmin Kim <whdalsrnt@megazone.com>
2021-05-23 16:33:39 +0900

    Add alert system's infos
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

---
+++ src/spaceone/monitoring/info/alert_info.py
@@ -0,0 +1,64 @@
+import functools
+from typing import List
+from spaceone.api.monitoring.v1 import alert_pb2
+from spaceone.core.pygrpc.message_type import *
+from spaceone.core import utils
+
+from spaceone.monitoring.model.alert_model import Alert, Responder
+
+__all__ = ['AlertInfo', 'AlertsInfo']
+
+
+def RespondersInfo(vos: List[Responder]):
+    if vos:
+        responders = []
+        for vo in vos:
+            info = {
+                'resource_type': vo.resource_type,
+                'resource_id': vo.resource_id
+            }
+            responder = alert_pb2.AlertResponder(**info)
+            responders.append(responder)
+
+        return responders
+    else:
+        return None
+
+
+def AlertInfo(alert_vo: Alert, minimal=False):
+    info = {
+        'alert_number': alert_vo.alert_number,
+        'alert_id': alert_vo.alert_id,
+        'title': alert_vo.title,
+        'state': alert_vo.state,
+        'status_message': alert_vo.status_message,
+        'assignee': alert_vo.assignee,
+        'urgency': alert_vo.urgency,
+        'escalation_level': alert_vo.escalation_level,
+        'project_id': alert_vo.project_id
+    }
+
+    if not minimal:
+        info.update({
+            'description': alert_vo.description,
+            'severity': alert_vo.severity,
+            'is_snoozed': alert_vo.is_snoozed,
+            'snoozed_end_time': utils.datetime_to_iso8601(alert_vo.snoozed_end_time),
+            'escalation_ttl': alert_vo.escalation_ttl,
+            'responders': RespondersInfo(alert_vo.responders),
+            'webhook_id': alert_vo.webhook_id,
+            'escalation_policy_id': alert_vo.escalation_policy_id,
+            'domain_id': alert_vo.domain_id,
+            'created_at': utils.datetime_to_iso8601(alert_vo.created_at),
+            'updated_at': utils.datetime_to_iso8601(alert_vo.updated_at),
+            'acknowledged_at': utils.datetime_to_iso8601(alert_vo.acknowledged_at),
+            'resolved_at': utils.datetime_to_iso8601(alert_vo.resolved_at),
+            'escalated_at': utils.datetime_to_iso8601(alert_vo.escalated_at)
+        })
+
+    return alert_pb2.AlertInfo(**info)
+
+
+def AlertsInfo(alert_vos, total_count, **kwargs):
+    return alert_pb2.AlertsInfo(results=list(
+        map(functools.partial(AlertInfo, **kwargs), alert_vos)), total_count=total_count)
--- src/spaceone/monitoring/info/project_alert_config_info.py
+++ src/spaceone/monitoring/info/project_alert_config_info.py
@@ -1,26 +1,30 @@
 import functools
 from spaceone.api.monitoring.v1 import project_alert_config_pb2
-from spaceone.core.pygrpc.message_type import *
 from spaceone.core import utils
-from spaceone.monitoring.model.project_alert_config_model import ProjectAlertConfig
+from spaceone.monitoring.model.project_alert_config_model import ProjectAlertConfig, NotificationOptions
 
-__all__ = ['ProjectAlertConfigInfo', 'ProjectAlertConfigsInfo', 'VerifyInfo']
+__all__ = ['ProjectAlertConfigInfo', 'ProjectAlertConfigsInfo']
+
+
+def NotificationOptionsInfo(vo: NotificationOptions):
+    if vo:
+        info = {
+            'urgency': vo.urgency
+        }
+
+        return project_alert_config_pb2.NotificationOptions(**info)
+    else:
+        return None
 
 
 def ProjectAlertConfigInfo(project_alert_config_vo: ProjectAlertConfig, minimal=False):
     info = {
-        'project_alert_config_id': project_alert_config_vo.project_alert_config_id,
-        'name': project_alert_config_vo.name,
-        'state': project_alert_config_vo.state,
-        'monitoring_type': project_alert_config_vo.monitoring_type,
-        'provider': project_alert_config_vo.provider
+        'project_id': project_alert_config_vo.project_id,
+        'notification_options': NotificationOptionsInfo(project_alert_config_vo.notification_options)
     }
 
     if not minimal:
         info.update({
-            'capability': change_struct_type(project_alert_config_vo.capability),
-            'plugin_info': PluginInfo(project_alert_config_vo.plugin_info),
-            'tags': change_struct_type(utils.tags_to_dict(project_alert_config_vo.tags)),
             'domain_id': project_alert_config_vo.domain_id,
             'created_at': utils.datetime_to_iso8601(project_alert_config_vo.created_at)
         })
@@ -31,7 +35,3 @@ def ProjectAlertConfigInfo(project_alert_config_vo: ProjectAlertConfig, minimal=
 def ProjectAlertConfigsInfo(project_alert_config_vos, total_count, **kwargs):
     return project_alert_config_pb2.ProjectAlertConfigsInfo(results=list(
         map(functools.partial(ProjectAlertConfigInfo, **kwargs), project_alert_config_vos)), total_count=total_count)
-
-
-def VerifyInfo(is_verify):
-    return project_alert_config_pb2.VerifyInfo(status=is_verify)
