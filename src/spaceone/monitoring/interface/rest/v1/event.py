2c6923fe7fc2c318e140429a49d96c53c0b96645
Jongmin Kim <whdalsrnt@megazone.com>
2021-06-16 20:32:40 +0900

    Add default response on event creation
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/monitoring/interface/rest/v1/event.py
+++ src/spaceone/monitoring/interface/rest/v1/event.py
@@ -14,8 +14,10 @@ async def webhook(webhook_id: str, access_key: str, request: Request):
     data = await request.json()
 
     event_service = locator.get_service('EventService')
-    return event_service.create({
+    event_service.create({
         'webhook_id': webhook_id,
         'access_key': access_key,
         'data': data or {}
     })
+
+    return {}
