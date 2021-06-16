11463d9ebfb5066e94419e8ee2c9c17e0951c4b8
Jongmin Kim <whdalsrnt@megazone.com>
2021-06-16 20:24:06 +0900

    Change event request body type
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/monitoring/interface/rest/v1/event.py
+++ src/spaceone/monitoring/interface/rest/v1/event.py
@@ -1,6 +1,5 @@
 import logging
-from typing import Dict, Any
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 from spaceone.core.locator import Locator
 
@@ -10,8 +9,10 @@ router = APIRouter()
 
 
 @router.post('/webhook/{webhook_id}/{access_key}/events')
-async def webhook(webhook_id: str, access_key: str, data: Dict[Any, Any] = None):
+async def webhook(webhook_id: str, access_key: str, request: Request):
     locator = Locator()
+    data = await request.json()
+
     event_service = locator.get_service('EventService')
     return event_service.create({
         'webhook_id': webhook_id,
