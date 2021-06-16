73e06fdc248ec4bc41b570be3b0b9d00f6aa435b
Jongmin Kim <whdalsrnt@megazone.com>
2021-06-16 17:56:45 +0900

    Add health check API
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/monitoring/interface/rest/router.py
+++ src/spaceone/monitoring/interface/rest/router.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from spaceone.monitoring.interface.rest.v1 import common
 from spaceone.monitoring.interface.rest.v1 import event
 
 app = FastAPI()
+app.include_router(common.router)
 app.include_router(
     event.router,
     prefix='/monitoring/v1'
