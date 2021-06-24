67d45415a39f72d4b10ae2b034178661399be392
Jongmin Kim <whdalsrnt@megazone.com>
2021-06-24 21:52:17 +0900

    Fix keyword search bug of alert
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/monitoring/service/alert_service.py
+++ src/spaceone/monitoring/service/alert_service.py
@@ -294,7 +294,7 @@ class AlertService(BaseService):
     @append_query_filter(['alert_number', 'alert_id', 'title', 'state', 'assignee', 'urgency', 'severity', 'is_snoozed',
                           'resource_id', 'webhook_id', 'escalation_policy_id', 'project_id', 'domain_id',
                           'user_projects'])
-    @append_keyword_filter(['alert_number', 'alert_id', 'title'])
+    @append_keyword_filter(['alert_id', 'title'])
     def list(self, params):
         """ List alerts
 
@@ -331,7 +331,7 @@ class AlertService(BaseService):
     })
     @check_required(['query', 'domain_id'])
     @append_query_filter(['domain_id', 'user_projects'])
-    @append_keyword_filter(['alert_number', 'alert_id', 'title'])
+    @append_keyword_filter(['alert_id', 'title'])
     def stat(self, params):
         """
         Args:
