4dde00ef2600a2e14552fa3d87022da0f348490d
Jongmin Kim <whdalsrnt@megazone.com>
2021-06-28 18:56:10 +0900

    Change short message format
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/monitoring/service/job_service.py
+++ src/spaceone/monitoring/service/job_service.py
@@ -383,10 +383,7 @@ class JobService(BaseService):
         description = alert_vo.description
 
         # TODO: Need to change multiple language
-        if 'name' in resource:
-            short_message = f'경고! 장애 발생. {resource["name"]}'
-        else:
-            short_message = f'경고! 장애 발생. {alert_vo.title}'
+        short_message = f'경고! 장애 발생! {tags["Project"].replace(">", "의")} 프로젝트에 장애가 발했습니다.'
 
         callbacks = []
 
