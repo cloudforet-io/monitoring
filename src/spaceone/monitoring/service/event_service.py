88c74af662ad383fbe1e23bda6f780c12b08a63b
Jongmin Kim <whdalsrnt@megazone.com>
2021-08-29 16:05:12 +0900

    fix: change project_alert_config's options
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/monitoring/service/event_service.py
+++ src/spaceone/monitoring/service/event_service.py
@@ -260,7 +260,7 @@ class EventService(BaseService):
     @cache.cacheable(key='auto-recovery:{domain_id}:{project_id}', expire=300)
     def _is_auto_recovery(self, project_id, domain_id):
         project_alert_config_vo: ProjectAlertConfig = self._get_project_alert_config(project_id, domain_id)
-        return project_alert_config_vo.options.auto_recovery
+        return project_alert_config_vo.options.recovery_mode == 'AUTO'
 
     def _get_project_alert_config(self, project_id, domain_id):
         project_alert_config_mgr: ProjectAlertConfigManager = self.locator.get_manager('ProjectAlertConfigManager')
