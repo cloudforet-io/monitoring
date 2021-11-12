18b2b7a22a478ed180ac26376124702d1f497809
jean1042 <jean1042@naver.com>
2021-11-12 16:40:30 +0900

    test: merge alerts test code

---
+++ test/factory/event_factory.py
@@ -0,0 +1,27 @@
+import factory
+
+from spaceone.core import utils
+from spaceone.monitoring.model.event_model import Event
+
+
+class EventFactory(factory.mongoengine.MongoEngineFactory):
+
+    class Meta:
+        model = Event
+
+    event_id = factory.LazyAttribute(lambda o: utils.generate_id('event'))
+    event_key = utils.random_string()
+    event_type = 'ALERT'
+    title = 'TRIGGERED'
+    description = factory.LazyAttribute(lambda o: utils.random_string())
+    urgency = 'HIGH'
+    severity = 'CRITICAL'
+    resource = {}
+    rule = utils.random_string()
+    raw_data = {}
+    alert_id = factory.LazyAttribute(lambda o: utils.generate_id('alert'))
+    webhook_id = factory.LazyAttribute(lambda  o: utils.generate_id('webhook'))
+    project_id = factory.LazyAttribute(lambda o: utils.generate_id('project'))
+    domain_id = utils.generate_id('domain', 10)
+    created_at = factory.Faker('date_time')
+    updated_at = factory.Faker('date_time')
--- test/service/test_alert_service.py
+++ test/service/test_alert_service.py
@@ -1,8 +1,6 @@
 import unittest
-from datetime import datetime, timedelta
 from unittest.mock import patch
 from mongoengine import connect, disconnect
-
 from spaceone.core.unittest.result import print_data
 from spaceone.core.unittest.runner import RichTestRunner
 from spaceone.core import config
@@ -11,15 +9,18 @@ from spaceone.core.transaction import Transaction
 from spaceone.monitoring.error import *
 from spaceone.monitoring.model.data_source_model import DataSource
 from spaceone.monitoring.service.alert_service import AlertService
+from spaceone.monitoring.service.event_service import EventService
+from spaceone.monitoring.manager.event_manager import EventManager
 from spaceone.monitoring.manager.alert_manager import AlertManager
 from spaceone.monitoring.manager.event_manager import EventManager
 from spaceone.monitoring.model.event_model import Event
 from spaceone.monitoring.info.alert_info import *
 from spaceone.monitoring.model.alert_model import *
 from test.factory.alert_factory import AlertFactory
+from test.factory.event_factory import EventFactory
 
 
-class TestMetricService(unittest.TestCase):
+class TestAlertService(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -43,63 +44,16 @@ class TestMetricService(unittest.TestCase):
     def tearDown(self, *args) -> None:
         print()
         print('(tearDown) ==> Delete all data_sources')
-        alert_vos = Alert.objects.filter()  # TODO : QUESTION
+        alert_vos = Alert.objects.filter()
         alert_vos.delete()
 
-    def _new_iter(self):
-        return
-
-    # TODO : QUESTION How to put factory generated data??? && how to put object
-    @patch.object(EventManager, 'list_events', return_value=[{"event_id": "event-94b6c77645d7", "alert_id": "alert-b7726200f8c2"}, {"event_id": "event-94b6c77645s4", "alert_id": "alert-b7726200f8c2"}])
-    @patch.object(EventManager, 'update_event_by_vo', return_value=({"event_id": "event-94b6c77645d7", "event_key": "296e44a16d1b10936f9d8587ce40efdc","alert_id": "alert-b0fd7e69691e"}))
+    @patch.object(EventManager, 'list_events', return_value=[Event(event_id='event-1130a81e7b64', alert_id='alert-400c20b10a5c'), Event(event_id='event-1130a81e7b64')])
+    @patch.object(EventManager, 'update_event_by_vo', return_value=Event(event_id='event-09729273f6c1', alert_id='alert-b02c18c373ae', event_type='ALERT'))
     @patch.object(AlertManager, 'merge_alerts', return_value=Alert())
-    def test_merge_alerts(self, mock_merge_alerts, *args):
-        '''
-        merge_to_alert_vo = AlertFactory()
-        one_alert_vo = AlertFactory()
-        two_alert_vo = AlertFactory()
-
-        one_alert_dict = one_alert_vo.to_dict()
-        two_alert_dict = two_alert_vo.to_dict()
-        '''
-
-        alert_one = {
-        "alert_id": "alert-b0fd7e69691e",
-        "title": "AWS SNS Parsing ERROR",
-        "state": "TRIGGERED",
-        "description": "TypeError(\"'NoneType' object is not iterable\")",
-        "urgency": "HIGH",
-        "severity": "CRITICAL",
-        "resource": {},
-        "additional_info": {}
-        }
-
-        alert_two = {
-            "alert_id": "alert-b0fd7e696913",
-            "title": "AWS SNS Parsing ERROR2",
-            "state": "TRIGGERED",
-            "description": "TypeError(\"'NoneType' object is not iterable\")",
-            "urgency": "HIGH",
-            "severity": "CRITICAL",
-            "resource": {},
-            "additional_info": {}
-        }
-
-        alerts = [alert_one, alert_two]
-        merge_to = 'alert-b0fd7e69691e'
-
-        mock_merge_alerts.return_value = {
-        'results': [{
-        "alert_id": "alert-b0fd7e69691e",
-        "title": "AWS SNS Parsing ERROR",
-        "state": "TRIGGERED",
-        "description": "TypeError(\"'NoneType' object is not iterable\")",
-        "urgency": "HIGH",
-        "severity": "CRITICAL",
-        "resource": {},
-        "additional_info": {}}]
-        }
-
+    @patch.object(AlertManager, 'delete_alert')
+    def test_merge_alerts(self, *args):
+        alerts = ['alert-400c20b10a5c', 'alert-b02c18c373ae']
+        merge_to = 'alert-b02c18c373ae'
         params = {
             'merge_to': merge_to,
             'alerts': alerts,
@@ -109,12 +63,15 @@ class TestMetricService(unittest.TestCase):
         self.transaction.method = 'merge'
         alert_svc = AlertService(transaction=self.transaction)
         alert_data_info = alert_svc.merge(params.copy())
+        AlertInfo(alert_data_info)
 
-        # print_data(alert_data_info, 'test_alert_merge')
-        print(f'alert_data_info: {alert_data_info}')
-        # AlertInfo(alert_data_info['results'][0])
+        # Test if updated event's id is equivalent with 'merge_to'
+        event_vo = EventFactory(alert_id=alert_data_info['alert_id'])
+        event_mgr = EventManager(transaction=self.transaction)
+        update_event_params = {'alert_id': merge_to}
+        updated_event = event_mgr.update_event_by_vo(self, params=update_event_params, event_vo=event_vo)
 
-        self.assertEqual(params['merge_to'], alert_data_info['results'][0]['alert_id'])
+        self.assertEqual(updated_event.alert_id, merge_to)
 
 
 if __name__ == "__main__":
