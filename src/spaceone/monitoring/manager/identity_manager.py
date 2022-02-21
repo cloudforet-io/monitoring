014495486903146b83577bc07b4af61c027a2f86
Jongmin Kim <whdalsrnt@megazone.com>
2022-02-21 11:21:27 +0900

    refactor: change console domain link by each domain
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/monitoring/manager/identity_manager.py
+++ src/spaceone/monitoring/manager/identity_manager.py
@@ -1,5 +1,6 @@
 import logging
 
+from spaceone.core import cache
 from spaceone.core.manager import BaseManager
 from spaceone.core.connector.space_connector import SpaceConnector
 
@@ -17,6 +18,9 @@ class IdentityManager(BaseManager):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.identity_connector: SpaceConnector = self.locator.get_connector('SpaceConnector', service='identity')
+
+    def get_domain(self, domain_id):
+        return self.identity_connector.dispatch('Domain.get', {'domain_id': domain_id})
 
     def get_user(self, user_id, domain_id):
         return self.identity_connector.dispatch('User.get', {'user_id': user_id, 'domain_id': domain_id})
