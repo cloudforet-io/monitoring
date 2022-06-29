26d321a488dd0f0ddba0cae10bcd4bf571d2e781
Jihyung Song <bluese@megazone.com>
2022-06-29 20:33:46 +0900

    refactor: update plugin metadata model

--- src/spaceone/monitoring/model/plugin_metadata_model.py
+++ src/spaceone/monitoring/model/plugin_metadata_model.py
@@ -1,16 +1,10 @@
 import jsonschema
-from schematics.models import Model
+from schematics import Model
 from schematics.exceptions import ValidationError
 from schematics.types import BaseType, ListType, DictType, StringType
 from schematics.types.compound import ModelType
 
 __all__ = ['MetricPluginMetadataModel', 'LogPluginMetadataModel']
-
-_SUPPORTED_RESOURCE_TYPE = [
-    'identity.ServiceAccount',
-    'inventory.Server',
-    'inventory.CloudService'
-]
 
 
 class JSONSchemaType(BaseType):
@@ -19,11 +13,6 @@ class JSONSchemaType(BaseType):
             jsonschema.Draft7Validator.check_schema(value)
         except Exception as e:
             raise ValidationError(key=f'Plugin metadata is invalid. (filter_format = {str(value)}')
-
-
-class ReferenceKeyModel(Model):
-    resource_type = StringType(required=True, choices=_SUPPORTED_RESOURCE_TYPE)
-    reference_key = StringType(required=True)
 
 
 class DynamicField(Model):
@@ -38,11 +27,10 @@ class TemplateModel(Model):
 
 
 class MetricPluginMetadataModel(Model):
-    supported_resource_type = ListType(StringType(choices=_SUPPORTED_RESOURCE_TYPE), required=True)
     supported_stat = ListType(StringType, required=True)
     required_keys = ListType(StringType)
 
 
 class LogPluginMetadataModel(Model):
-    supported_resource_type = ListType(StringType(choices=_SUPPORTED_RESOURCE_TYPE), required=True)
     required_keys = ListType(StringType)
+    view = DictType(BaseType)
