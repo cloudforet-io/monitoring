84b0c17e25f9fb58e0ee0ed52f79cfa497a9d25e
Jongmin Kim <whdalsrnt@megazone.com>
2022-07-11 16:41:21 +0900

    fix: fix redirect url bug
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/monitoring/interface/rest/v1/alert.py
+++ src/spaceone/monitoring/interface/rest/v1/alert.py
@@ -2,10 +2,11 @@ import logging
 from fastapi import APIRouter, Request, HTTPException
 
 from spaceone.core.error import *
-from spaceone.core import config
+from spaceone.core import config, cache
 from spaceone.core.locator import Locator
 from spaceone.monitoring.model import Alert
 from spaceone.monitoring.service import AlertService
+from spaceone.monitoring.manager import IdentityManager
 from fastapi.responses import RedirectResponse
 
 _LOGGER = logging.getLogger(__name__)
@@ -15,13 +16,18 @@ router = APIRouter()
 
 @router.get('/alert/{alert_id}/{access_key}/{state}')
 async def update_alert_state_get(alert_id: str, access_key: str, state: str):
-    _update_alert_state(alert_id, access_key, state)
+    locator = Locator()
 
-    return _make_redirect_response(alert_id)
+    alert_info = _update_alert_state(locator, alert_id, access_key, state)
+    domain_name = _get_domain_name(locator, alert_info['domain_id'])
+
+    return _make_redirect_response(alert_id, domain_name)
 
 
 @router.post('/alert/{alert_id}/{access_key}/{state}')
 async def update_alert_state_post(alert_id: str, access_key: str, state: str, request: Request):
+    locator = Locator()
+
     try:
         data = await request.json()
     except Exception as e:
@@ -29,15 +35,16 @@ async def update_alert_state_post(alert_id: str, access_key: str, state: str, re
         data = {}
 
     if data.get('code') != 'TIME_OUT':
-        _update_alert_state(alert_id, access_key, state)
+        alert_info = _update_alert_state(locator, alert_id, access_key, state)
+        domain_name = _get_domain_name(locator, alert_info['domain_id'])
 
-    return _make_redirect_response(alert_id)
+        return _make_redirect_response(alert_id, domain_name)
+    else:
+        raise HTTPException(status_code=403, detail='Timeout!')
 
 
-def _update_alert_state(alert_id, access_key, state):
-    locator = Locator()
+def _update_alert_state(locator, alert_id, access_key, state):
     try:
-
         alert_service: AlertService = locator.get_service('AlertService')
         alert_vo: Alert = alert_service.update_state({
             'alert_id': alert_id,
@@ -51,7 +58,8 @@ def _update_alert_state(alert_id, access_key, state):
             'title': alert_vo.title,
             'state': alert_vo.state,
             'assignee': alert_vo.assignee,
-            'urgency': alert_vo.urgency
+            'urgency': alert_vo.urgency,
+            'domain_id': alert_vo.domain_id
         }
 
     except ERROR_BASE as e:
@@ -60,10 +68,22 @@ def _update_alert_state(alert_id, access_key, state):
         raise HTTPException(status_code=500, detail=f'Unknown Error: {str(e)}')
 
 
-def _make_redirect_response(alert_id):
+@cache.cacheable(key='domain-name:{domain_id}', expire=3600)
+def _get_domain_name(locator, domain_id):
+    try:
+        identity_mgr: IdentityManager = locator.get_manager('IdentityManager')
+        domain_info = identity_mgr.get_domain(domain_id)
+        return domain_info['name']
+    except Exception as e:
+        _LOGGER.error(f'[_get_domain_name] Failed to get domain: {e}', exc_info=True)
+        raise HTTPException(status_code=500, detail=f'Unknown Error: {str(e)}')
+
+
+def _make_redirect_response(alert_id, domain_name):
     console_domain = config.get_global('CONSOLE_DOMAIN')
 
     if console_domain.strip() != '':
+        console_domain = console_domain.format(domain_name=domain_name)
         return RedirectResponse(f'{console_domain}/alert-manager/alert/{alert_id}')
     else:
         return None
