957e2486f9a39c5b72e3b2a0eb29aab47666f6ea
seolmin <seolmin@megazone.com>
2023-06-29 00:27:40 +0900

    fix: change so that TOKEN can be obtained from GLOBAL when using SpaceConnector in worker

--- src/spaceone/monitoring/manager/notification_manager.py
+++ src/spaceone/monitoring/manager/notification_manager.py
@@ -1,5 +1,6 @@
 import logging
 
+from spaceone.core import config
 from spaceone.core.manager import BaseManager
 from spaceone.core.connector.space_connector import SpaceConnector
 
@@ -11,7 +12,8 @@ class NotificationManager(BaseManager):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.notification_connector: SpaceConnector = self.locator.get_connector('SpaceConnector',
-                                                                                 service='notification')
+                                                                                 service='notification',
+                                                                                 token=config.get_global('TOKEN'))
 
     def create_notification(self, message):
         _LOGGER.debug(f'Notify message: {message}')
