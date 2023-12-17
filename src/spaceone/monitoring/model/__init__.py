eaec8cba34a01a985b972925d950e75d0caae49a
seolmin <seolmin@megazone.com>
2023-12-17 17:43:12 +0900

    fix: remove maintenance_window feature in monitoring service

--- src/spaceone/monitoring/model/__init__.py
+++ src/spaceone/monitoring/model/__init__.py
@@ -1,11 +1,10 @@
+from spaceone.monitoring.model.alert_model import Alert, AlertNumber
 from spaceone.monitoring.model.data_source_model import DataSource
+from spaceone.monitoring.model.escalation_policy_model import EscalationPolicy
+from spaceone.monitoring.model.event_model import Event
+from spaceone.monitoring.model.event_rule_model import EventRule
+from spaceone.monitoring.model.job_model import Job
+from spaceone.monitoring.model.note_model import Note
 from spaceone.monitoring.model.plugin_metadata_model import *
 from spaceone.monitoring.model.project_alert_config_model import ProjectAlertConfig
-from spaceone.monitoring.model.escalation_policy_model import EscalationPolicy
-from spaceone.monitoring.model.event_rule_model import EventRule
 from spaceone.monitoring.model.webhook_model import Webhook
-from spaceone.monitoring.model.maintenance_window_model import MaintenanceWindow
-from spaceone.monitoring.model.alert_model import Alert, AlertNumber
-from spaceone.monitoring.model.note_model import Note
-from spaceone.monitoring.model.event_model import Event
-from spaceone.monitoring.model.job_model import Job
