c6ee1e5523cfff0b3f3ff980d3c91d3c9bf9fa6f
seolmin <seolmin@megazone.com>
2023-12-25 18:03:09 +0900

    fix: change get token method in notification manager

--- src/spaceone/monitoring/manager/notification_manager.py
+++ src/spaceone/monitoring/manager/notification_manager.py
@@ -11,12 +11,16 @@ class NotificationManager(BaseManager):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.notification_connector: SpaceConnector = self.locator.get_connector(
-            "SpaceConnector", service="notification", token=config.get_global("TOKEN")
+            "SpaceConnector", service="notification"
         )
 
-    def create_notification(self, message, domain_id):
+    def create_notification(self, message: dict, domain_id: str) -> dict:
         _LOGGER.debug(f"Notify message: {message}")
+        _LOGGER.debug(f"[TEST]Notify domain_id: {domain_id}")
         system_token = config.get_global("TOKEN")
         return self.notification_connector.dispatch(
-            "Notification.create", message, token=system_token, x_domain_id=domain_id
+            "Notification.create",
+            message,
+            token=system_token,
+            x_domain_id=domain_id,
         )
