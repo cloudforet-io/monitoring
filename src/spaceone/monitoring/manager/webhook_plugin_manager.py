0847c2d9781d0f87876fbbca03db4184975c09e4
seolmin <seolmin@megazone.com>
2024-01-05 14:31:43 +0900

    fix: add no_token in webhook manager and data source plugin manager

--- src/spaceone/monitoring/manager/webhook_plugin_manager.py
+++ src/spaceone/monitoring/manager/webhook_plugin_manager.py
@@ -9,19 +9,19 @@ _LOGGER = logging.getLogger(__name__)
 class WebhookPluginManager(BaseManager):
     def init_plugin(self, endpoint, options):
         plugin_connector: SpaceConnector = self.locator.get_connector(
-            "SpaceConnector", endpoint=endpoint
+            "SpaceConnector", endpoint=endpoint, token="NO_TOKEN"
         )
         return plugin_connector.dispatch("Webhook.init", {"options": options})
 
     def verify_plugin(self, endpoint, options):
         plugin_connector: SpaceConnector = self.locator.get_connector(
-            "SpaceConnector", endpoint=endpoint
+            "SpaceConnector", endpoint=endpoint, token="NO_TOKEN"
         )
         plugin_connector.dispatch("Webhook.verify", {"options": options})
 
     def parse_event(self, endpoint, options, data):
         plugin_connector: SpaceConnector = self.locator.get_connector(
-            "SpaceConnector", endpoint=endpoint
+            "SpaceConnector", endpoint=endpoint, token="NO_TOKEN"
         )
 
         params = {
