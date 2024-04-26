a93f4850b1f1047c943c33cd0f9dc6455ec09b6c
Youngjin Jo <youngjinjo@megazone.com>
2024-04-26 15:53:39 +0900

    chore: add Body of FastAPI for local test
    Signed-off-by: Youngjin Jo <youngjinjo@megazone.com>

--- src/spaceone/monitoring/interface/rest/v1/event.py
+++ src/spaceone/monitoring/interface/rest/v1/event.py
@@ -2,11 +2,8 @@ import logging
 from fastapi import Request, Body
 from fastapi_utils.inferring_router import InferringRouter
 from fastapi_utils.cbv import cbv
-from spaceone.core.fastapi.api import BaseAPI, exception_handler
-from spaceone.core.error import *
-from spaceone.core.locator import Locator
+from spaceone.core.fastapi.api import BaseAPI
 from spaceone.monitoring.service import EventService
-from spaceone.monitoring.model.event_model import CreateEvent
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -16,7 +13,9 @@ router = InferringRouter()
 @cbv(router)
 class Event(BaseAPI):
     @router.post("/webhook/{webhook_id}/{access_key}/events")
-    async def create_event(self, access_key: str, webhook_id: str, request: Request):
+    async def create_event(
+        self, access_key: str, webhook_id: str, request: Request, data: dict = Body()
+    ):
         params, metadata = await self.parse_request(request)
 
         event_service: EventService = self.locator.get_service("EventService")
